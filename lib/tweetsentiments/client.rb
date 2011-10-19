ee9c24e4dd30c48237e62d5ef541f8e15ec344d8
Tom Zeng <tom@intridea.com>
2011-10-19 11:54:00 -0400

    fixed nil.[] on classify and classify_simple

--- lib/tweetsentiments/client.rb
+++ lib/tweetsentiments/client.rb
@@ -2,11 +2,11 @@ module Tweetsentiments
   class Client < API
     def classify(text, options={})
       response = get("classify", options.merge(:text => text))
-      {:sentiment=>response["sentiment"], :translated_text=>response["translate_text"].to_s } 
+      {:sentiment=>response["sentiment"], :translated_text=>response["translate_text"].to_s } if response 
     end
     def classify_simple(text, options={})
       response = get("classify_simple", options.merge(:text => text))
-      response["value"]
+      response["value"] if response
     end
     def analyze(text, options={})
       response = get("analyze", options.merge(:text => text))
