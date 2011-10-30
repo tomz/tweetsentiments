fbb431cd3e011aed869faf03c5ce5c2238532be3
Tom Zeng <tom@intridea.com>
2011-10-30 02:40:53 -0400

    added the langauge detection api

--- lib/tweetsentiments/client.rb
+++ lib/tweetsentiments/client.rb
@@ -8,6 +8,9 @@ module Tweetsentiments
       response = get("classify_simple", options.merge(:text => text))
       response["value"] if response
     end
+    def lang_detect(text, options={})
+      response = get("lang/detect", options.merge(:text => text))
+    end
     def analyze(text, options={})
       response = get("analyze", options.merge(:text => text))
     end
