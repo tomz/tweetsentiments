260082f51d8940c797ebfdc429b667d6814398e8
Tom Zeng <tom@intridea.com>
2011-11-20 15:57:51 -0500

    version 0.2.1 pointing to the new api server

--- lib/tweetsentiments/client.rb
+++ lib/tweetsentiments/client.rb
@@ -1,21 +1,47 @@
 module Tweetsentiments
   class Client < API
-    def classify(text, options={})
-      response = get("classify", options.merge(:text => text))
-      {:sentiment=>response["sentiment"], :translated_text=>response["translate_text"].to_s } if response 
+    @token = nil
+    
+    def token
+      @token
     end
-    def classify_simple(text, options={})
-      response = get("classify_simple", options.merge(:text => text))
-      response["value"] if response
+    
+    def login(username, password)
+      response = get("login", {:username=>username, :password=>password})
+      @token = response["token"]
     end
+    
+    def rate_limit(options={})
+      response = get("rate_limit", options)
+    end
+    
+    def classify(text, type=:sentiment, options={})
+      response = get("classify/type", options.merge(:text => text))
+    end
+    
+    def classify_simple(text, type=:sentiment, options={})
+      response = get("classify_simple/type", options.merge(:text => text))
+    end
+    
+    def classify_all(text, options={})
+      response = get("classify_all", options.merge(:text => text))
+    end
+    
     def lang_detect(text, options={})
       response = get("lang/detect", options.merge(:text => text))
     end
+    
+    def lang_detect_multi(text, options={})
+      response = get("lang/detect_multi", options.merge(:text => text))
+    end
+    
     def analyze(text, options={})
       response = get("analyze", options.merge(:text => text))
     end
+    
     def search(options={})
       response = get("search", options)
     end
+    
   end
 end
--- tweetsentiments.gemspec
+++ tweetsentiments.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{tweetsentiments}
-  s.version = "0.1.1"
+  s.version = "0.2.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Yong Gu", "Tom Zeng"]
-  s.date = %q{2011-10-30}
+  s.date = %q{2011-11-20}
   s.description = %q{A Ruby wrapper for the www.tweetsentiments.com REST APIs}
   s.email = %q{tom@intridea.com, yonggu@intridea.com}
   s.extra_rdoc_files = [
